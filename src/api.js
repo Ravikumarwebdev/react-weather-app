0e36955cd49dd3fe72e7ac30d8988f78c896f69c
Ravikumarwebdev <chennaiyanravikumar96@gmail.com>
2022-07-22 12:09:12 +0530

    weather-app

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '1e4e041749msh21d068e36dadebep10fe46jsnbcb881178c0e',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
+
+export const WEATHER_API_KEY = '6f55ca7af689e89e70fa174cf82c5c19'
